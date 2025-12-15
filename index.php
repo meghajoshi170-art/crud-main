5703c652a7bdad622f94f12fdb07927b2e9987de
meghajoshi170-art <megha.joshi170@gmail.com>
2025-12-15 10:47:09 -0800

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
 	<h1>MySQL Table Viewer</h1>
 	<?php
 		// Define database connection variables
-		$servername = "DBServer";
-		$username = "DB_USER";
-		$password = "DB_PASSWORD";
-		$dbname = "DB_NAME";
+		$servername = "employeesmysql123.mysql.database.azure.com";
+		$username = "adminmysql";
+		$password = "Ohmygod87";
+		$dbname = "employeesdb";
 
 		// Create database connection
 		$conn = new mysqli($servername, $username, $password, $dbname);
